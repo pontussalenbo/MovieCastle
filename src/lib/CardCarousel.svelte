e8e995130bfc01710a1321751883ecbdc9214c20
Pontus Salenbo <pontuss98@hotmail.com>
2023-02-28 13:07:34 +0100

    Add carousel for movies.

---
+++ src/lib/CardCarousel.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	// import Carousel from 'svelte-carousel';
+	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+	import '@splidejs/svelte-splide/css';
+	import MovieCard from './MovieCard.svelte';
+	let carousel;
+	export let movies: any[];
+	export let title: string;
+	const handleNextClick = () => {
+		carousel.goToNext();
+	};
+</script>
+
+<div class="min-h-md">
+	<div class="text-4xl font-medium mt-4">
+		{title}
+	</div>
+	<Splide
+		options={{ perPage: Math.min(movies.length, 5), perMove: 1, gap: '5rem', rewind: true }}
+		class="flex flex-row"
+	>
+		{#each movies as movie}
+			<SplideSlide>
+				<MovieCard {movie} />
+			</SplideSlide>
+		{/each}
+	</Splide>
+</div>
---
+++ src/lib/MovieCard.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Card, ButtonGroup, Button } from 'flowbite-svelte';
+	export let movie: any;
+
+	const handleAdd = () => {};
+</script>
+
+<div class="mt-2 max-w-4xl">
+	<Card img={movie.poster} href={'/movies/' + movie.id}>
+		<h3 class="text-xl font-bold">{movie.title}</h3>
+		<div class="mt-2 flex flex-row">
+			<p>{movie.genres.join(', ')}</p>
+		</div>
+		<div>
+			<Button on:click={handleAdd} outline color="green">Add to MovieCastle</Button>
+		</div>
+	</Card>
+</div>
